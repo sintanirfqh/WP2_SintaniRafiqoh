c95fdc85225d679dba7b3cf0888c4c91fb77406a
Sintani Rafiqoh <91405991+sintanirfqh@users.noreply.github.com>
2021-09-26 18:19:10 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hai</h1>";
+        echo "Saya Sintani Rafiqoh, 
+        saya tinggal di Cibarusah. 
+        Hobi saya adalah menulis dan menggambar.";
+    }
+}
