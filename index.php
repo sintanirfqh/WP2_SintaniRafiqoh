b73dc0723ee07ae00ec3385d705eaa0fce09e431
Sintani Rafiqoh <91405991+sintanirfqh@users.noreply.github.com>
2021-12-21 10:21:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-</div>
-<!-- /.container-fluid -->
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <p class="card-text"></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
